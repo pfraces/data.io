d8a964f4ccaba183d10ce476e4d90449b408c3e2
Pau Fracés <pfraces@gmail.com>
2023-12-07 22:00:51 +0100

    added CSS architecture

---
+++ src/styles/styles.js
@@ -0,0 +1,9 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './reset.css';
+import './layout.css';
+import './colors.css';
+import './theme.css';
+import './overrides.css';
