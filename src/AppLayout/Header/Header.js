f1218908a9d3fa6526ca01584f4dd96e4e2b98ba
Pau Fracés <pfraces@gmail.com>
2023-12-09 00:59:55 +0100

    added session widget at header

--- src/AppLayout/Header/Header.js
+++ src/AppLayout/Header/Header.js
@@ -1,7 +1,41 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Divider,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useAuth } from 'src/firebase/auth';
 import './Header.css';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+  const [accountMenuAnchor, setAccountMenuAnchor] = useState(null);
+
+  const onAccountMenuOpen = (event) => {
+    setAccountMenuAnchor(event.currentTarget);
+  };
+
+  const closeAccountMenu = () => {
+    setAccountMenuAnchor(null);
+  };
+
+  const onProfile = () => {
+    navigate('profile');
+    closeAccountMenu();
+  };
+
+  const onLogout = () => {
+    closeAccountMenu();
+    logout();
+  };
+
   return (
     <div className="Header">
       <div className="title">
@@ -15,9 +49,47 @@ export default function Header() {
       </div>
 
       <div className="session">
-        <Link to="login" className="link nowrap">
-          Log in
-        </Link>
+        {!user && (
+          <Link to="login" className="link nowrap">
+            Log in
+          </Link>
+        )}
+
+        {user && (
+          <>
+            <IconButton
+              size="large"
+              className="account-menu-toggler"
+              onClick={onAccountMenuOpen}
+              color="inherit"
+            >
+              <Avatar className="avatar" />
+            </IconButton>
+
+            <Menu
+              anchorEl={accountMenuAnchor}
+              open={Boolean(accountMenuAnchor)}
+              onClose={closeAccountMenu}
+              PaperProps={{ className: 'account-menu' }}
+            >
+              <MenuItem onClick={onProfile}>
+                <ListItemIcon>
+                  <AccountCircleIcon fontSize="small" />
+                </ListItemIcon>
+                Profile
+              </MenuItem>
+
+              <Divider />
+
+              <MenuItem onClick={onLogout}>
+                <ListItemIcon>
+                  <LogoutIcon fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
+          </>
+        )}
       </div>
     </div>
   );
