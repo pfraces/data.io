2f19a505e32d7d10a64ffed72096329e211aa133
Pau Fracés <pfraces@gmail.com>
2023-12-10 21:04:15 +0100

    added responsive hamburger menu

--- src/AppLayout/Header/Header.js
+++ src/AppLayout/Header/Header.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import AppMenu from './AppMenu/AppMenu';
 import SessionWidget from './SessionWidget/SessionWidget';
 import './Header.css';
 
 export default function Header() {
   return (
     <div className="Header">
+      <div className="app-menu-toggler">
+        <AppMenu />
+      </div>
+
       <div className="title">
         <h1>data.io</h1>
       </div>
